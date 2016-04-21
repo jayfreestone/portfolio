fd9015857aa4dcfdefd0f41b287a61a75b1e0248
Jay Freestone <mail@jayfreestone.com>
2016-04-21 20:55:34 +0100

    Restyles about page.

--- wp-content/themes/jf/page-about.php
+++ wp-content/themes/jf/page-about.php
@@ -1,36 +1,30 @@
 <?php get_header(); ?>
 <div class="l-content">
-	<div class="l-container">
-		<h1 class="heading-1"><?php the_title(); ?></h1>
+	<div class="l-container l-container--full">
 
 		<div class="about cf">
+			<div class="about__content">
+			   <h1 class="heading-1"><?php the_title(); ?></h1>
+			   <?php the_field( 'page_content' ); ?>
+			</div>
+
 			<?php 
 			$photo = get_field( 'photo' );
 			$widePhoto = get_field( 'photo_wide' );
 			?>
-			
-			<picture class="about__image">
-				<source data-srcset="<?php echo $photo['url']; ?>" media="(min-width: 60em)">
-				<source data-srcset="<?php echo $widePhoto['url']; ?>">
-				<img class="lazyload" data-src="<?php echo $widePhoto['url'] ?>" alt="Photo of Jay Freestone">
-			</picture>
 
-			<div class="about__content">
-				<?php the_field( 'page_content' ); ?>
+			<div class="about__image">
+			   <div class="image-wrapper image-wrapper--about">
+			      <picture>
+			         <source data-srcset="<?php echo $widePhoto['url']; ?>" media="(min-width: 75em)">
+			         <source data-srcset="<?php echo $photo['url']; ?>" media="(min-width: 60em)">
+			         <source data-srcset="<?php echo $widePhoto['url']; ?>" media="(min-width: 32em)">
+			         <source data-srcset="<?php echo $photo['url']; ?>">
+			         <img class="lazyload" data-src="<?php echo $photo['url'] ?>" alt="Photo of Jay Freestone">
+			      </picture>
+			   </div>
 			</div>
-			
-			<div class="about__currently">
-				<?php if ( have_rows( 'currently' ) ) : ?>
-					<ul class="link-widget">
-					<?php while ( have_rows( 'currently' ) ) : the_row(); ?>
-						<li>
-							<h3 class="heading-3"><?php the_sub_field( 'type' ); ?></h3>
-							<?php the_sub_field( 'thing' ); ?>
-						</li>
-					<?php endwhile; ?>
-					</ul>
-				<?php endif; ?>
-			</div>
+
 			
 		</div>
 	</div>
