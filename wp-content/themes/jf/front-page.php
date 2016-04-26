db1e9b0c3b4c7c9641b5d527492efd97cc2c0694
Jay Freestone <jfree@Helens-MacBook-Pro.local>
2016-04-26 12:33:42 +0100

    Fixes snake case PHP variable.

--- wp-content/themes/jf/front-page.php
+++ wp-content/themes/jf/front-page.php
@@ -2,7 +2,7 @@
 
 	<?php
 	$work = get_recent_work();
-	$workCount = $work->post_count;
+	$work_count = $work->post_count;
 	?>
 
 	<div class="work-preview-container">
@@ -11,7 +11,7 @@
 
 		<nav class="work-preview-container__nav">
 			<ul>
-				<?php for ( $x = 0; $x < $workCount; $x++ ) : ?>
+				<?php for ( $x = 0; $x < $work_count; $x++ ) : ?>
 					<li class="work-preview-container__nav__item"><a href="work-preview--<?php echo esc_html( $x + 1 ); ?>">0<?php echo esc_html( $x + 1 ); ?></a></li>
 				<?php endfor ?>
 			</ul>
@@ -23,28 +23,28 @@
 
 			<?php
 			$i++;
-			$workClass = 'work-preview';
-			$workClass .= ' work-preview--' . $i;
+			$work_class = 'work-preview';
+			$work_class .= ' work-preview--' . $i;
 
 			if ( get_field( 'whiteout' ) ) {
-				$workClass .= ' work-preview--whiteout';
+				$work_class .= ' work-preview--whiteout';
 			}
 
 			if ( get_field( 'text_on_right' ) ) {
-				$workClass .= ' work-preview--text-on-right';
+				$work_class .= ' work-preview--text-on-right';
 			}
 
 			if ( $i >= 2 ) {
-				$workClass .= ' work-preview--next';
+				$work_class .= ' work-preview--next';
 			}
 		   	?>
 
-			<article id="work-preview--<?php echo esc_html( $i ); ?>" class="<?php echo esc_html( $workClass ); ?>" style="background-color: <?php the_field( 'homepage_background_color' ); ?>" itemscope itemtype="http://schema.org/CreativeWork">
+			<article id="work-preview--<?php echo esc_html( $i ); ?>" class="<?php echo esc_html( $work_class ); ?>" style="background-color: <?php the_field( 'homepage_background_color' ); ?>" itemscope itemtype="http://schema.org/CreativeWork">
 				<div class="work-preview__copy">
 					<a href="<?php the_permalink(); ?>">
 						<h2 class="heading-1 work-preview__title" itemprop="name"><?php the_title(); ?></h2>
 						<span itemprop="description"><?php the_field( 'homepage_intro' ); ?></span>
-						<span href="<?php the_permalink(); ?>" class="work-preview__link">
+						<span class="work-preview__link">
 							View Work
 						</span>
 					</a>
@@ -52,8 +52,8 @@
 
 				<div class="work-preview__image">
 					<?php
-					$workImage = get_field( 'homepage_image' );
-					$homepage = $workImage['sizes']['homepage'];
+					$work_image = get_field( 'homepage_image' );
+					$homepage = $work_image['sizes']['homepage'];
 					?>
 				</div>
 			</article>
