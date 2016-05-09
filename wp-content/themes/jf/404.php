7e318e5811546f4842bb7ee5718b13e49cbff9d8
Jay Freestone <jay.freestone@overture.london>
2016-05-09 17:07:17 +0100

    Improves the appearance of 404 page.

--- wp-content/themes/jf/404.php
+++ wp-content/themes/jf/404.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 <div class="l-container">
-	<h1>404</h1>
-	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fourofour.png" alt="Dead Jay">
-	<p>Unfortunately the page could not be found.</p>
+	<div class="l-content status">
+		<h1 class="heading-1">404</h1>
+		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fourofour.png" alt="Dead Jay">
+	</div>
 </div>
 <?php get_footer(); ?>
